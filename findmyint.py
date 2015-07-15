4f2a4cd0033175e612de601780ecf202859c919f
IrSent <my.irsent@gmail.com>
2015-07-15 12:26:03 +0300

    recursive ex completed

--- findmyint.py
+++ findmyint.py
@@ -1,42 +1,36 @@
 # -*- coding: utf-8 -*-
 #
-#def binarySearch(array, number):
-#    if len(array) == 0:
-#        return False
-#    else:
-#        mid = len(array) // 2
-#        if array[mid] == number:
-#          return True
-#        else:
-#          if number < array[mid]:
-#            return binarySearch(array[:mid], number)
-#          else:
-#            return binarySearch(array[mid+1:], number)
-#
+import sys
+sys.setrecursionlimit(10000)
+
+def recursiveBinarySearch(array, number, low_index, high_index):
+	if high_index < low_index:
+		return -1
+	else:
+		mid = (low_index + high_index) / 2
+		if array[mid] == number:
+			return mid
+		else:
+			if number < array[mid]:
+				high_index = mid - 1
+				return recursiveBinarySearch(array, number, low_index, high_index)
+			else:
+				low_index = mid + 1
+				return recursiveBinarySearch(array, number, low_index, high_index)
+
 def binarySearch(array, number):
 	first = 0
 	last = len(array) - 1
-	found = False
-	howManyTimes = 0
-	while first <= last and not found:
-		mid = (first + last) // 2
-		howManyTimes += 1
-		print howManyTimes
+	while first <= last:
+		mid = (first + last) / 2
 		if array[mid] == number:
-			howManyTimes += 1
-			print howManyTimes
-			print mid
-			found = True
+			return mid
 		else:
 			if number < array[mid]:
 				last = mid - 1
-				howManyTimes += 1
-				print howManyTimes
 			else:
 				first = mid + 1
-				howManyTimes += 1
-				print howManyTimes
-	return found
+	return -1
 
 num = -1
 while num < 0:
@@ -47,3 +41,4 @@ with open('output.txt', 'r') as f:
 	arr = map(int, f.readline().strip().split())
 
 print binarySearch(arr, num)
+print recursiveBinarySearch(arr, num, 0, len(arr))
